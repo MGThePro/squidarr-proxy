7e8744c153a4557566be355380f8cc4eeb065a66
mgthepro <17652761+MGThePro@users.noreply.github.com>
2025-06-23 20:45:29 +0200

    fixes to downloader

--- src/downloader.go
+++ src/downloader.go
@@ -5,9 +5,11 @@ import (
 	"github.com/cavaliergopher/grab/v3"
 	"github.com/tidwall/gjson"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
@@ -64,7 +66,7 @@ func get_config(w http.ResponseWriter, u url.URL) {
 	w.Write([]byte(`{
 	    "config": {
 	        "misc": {
-	            "complete_dir": "/data/complete",
+	            "complete_dir": "` + DownloadCompletePath + `",
 	            "enable_tv_sorting": false,
 	            "enable_movie_sorting": false,
 	            "pre_check": false,
@@ -76,7 +78,7 @@ func get_config(w http.ResponseWriter, u url.URL) {
 	                "name": "music",
 	                "pp": "",
 	                "script": "Default",
-	                "dir": "/data/complete/music",
+	                "dir": "` + DownloadIncompletePath + `/music",
 	                "priority": -100
 	            },
 	        ],
@@ -273,9 +275,132 @@ func startDownload(download *Download) {
 		}
 	}
 	//Download (should be) complete, move to complete folder
-	err = os.Rename(DownloadIncompletePath+"/"+Category+"/"+download.FileName, DownloadCompletePath+"/"+Category+"/"+download.FileName)
+	err = RenameDir(DownloadIncompletePath+"/"+Category+"/"+download.FileName, DownloadCompletePath+"/"+Category+"/"+download.FileName, false)
 	if err != nil {
 		fmt.Println("Couldn't move folder to destination")
 		fmt.Println(err)
 	}
+}
+
+func RenameDir(src string, dst string, force bool) (err error) {
+	err = CopyDir(src, dst, force)
+	if err != nil {
+		return fmt.Errorf("failed to copy source dir %s to %s: %s", src, dst, err)
+	}
+	err = os.RemoveAll(src)
+	if err != nil {
+		return fmt.Errorf("failed to cleanup source dir %s: %s", src, err)
+	}
+	return nil
+}
+func RenameFile(src string, dst string) (err error) {
+	err = CopyFile(src, dst)
+	if err != nil {
+		return fmt.Errorf("failed to copy source file %s to %s: %s", src, dst, err)
+	}
+	err = os.RemoveAll(src)
+	if err != nil {
+		return fmt.Errorf("failed to cleanup source file %s: %s", src, err)
+	}
+	return nil
+}
+
+// credit https://gist.github.com/r0l1/92462b38df26839a3ca324697c8cba04
+func CopyDir(src string, dst string, force bool) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !si.IsDir() {
+		return fmt.Errorf("source is not a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err == nil {
+		if force {
+			os.RemoveAll(dst)
+		} else {
+			return fmt.Errorf("destination already exists")
+		}
+	}
+
+	err = os.MkdirAll(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			err = CopyDir(srcPath, dstPath, force)
+			if err != nil {
+				return
+			}
+		} else {
+			// Skip symlinks.
+			if entry.Mode()&os.ModeSymlink != 0 {
+				continue
+			}
+
+			err = CopyFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+// credit https://gist.github.com/r0l1/92462b38df26839a3ca324697c8cba04
+func CopyFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	return
 }
